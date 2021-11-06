4a2049e3b3c73aaa970d68f4654ceea80cade6dc
CJ Bayron <cjdbayron@gmail.com>
2021-11-07 04:55:05 +0800

    Updates

---
+++ precache-manifest.fa232c10e720a3fb4621b79b306b0638.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78499d08c1b57afc2f06be22aef08bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0993734bf047e8fbf529",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "1a9c07f278f4d52d3070",
+    "url": "/static/js/2.3746c49a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3746c49a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0993734bf047e8fbf529",
+    "url": "/static/js/main.b2f43a07.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
